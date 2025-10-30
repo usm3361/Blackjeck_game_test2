6c00ca96ee3d5ef5c936f07d8d57f43cc0b0bc7b
Uri Miller <usm3361@gmail.com>
2025-10-30 13:02:40 +0200

    done test

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from core.game_logic import run_full_game
 from core.deck import shuffle_by_suit, build_standard_deck
 
-
 if __name__ == "__main__":
     deck = build_standard_deck()
-    shuffle_by_suit(deck)
+    shuffle_deck= shuffle_by_suit(deck)
 
     player = {"hand": [ ] }
     dealer = {"hand": [ ] }
 
-    run_full_game(deck, player, dealer)
+    run_full_game(shuffle_deck, player, dealer)
 
 
